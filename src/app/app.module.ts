91912a833daefc49f8a9923a5c0a2a1b0ce089c7
Sthe12 <sthevven.morales@epn.edu.ec>
2023-02-16 18:22:25 -0500

    agrego frontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { PieComponent } from './components/pie/pie.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { LibrosComponent } from './components/libros/libros.component';
+import { CrearlibroComponent } from './components/crearlibro/crearlibro.component';
+import { DetallelibroComponent } from './components/detallelibro/detallelibro.component';
+import { EditarlibroComponent } from './components/editarlibro/editarlibro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EncabezadoComponent,
+    PieComponent,
+    ContactoComponent,
+    LibrosComponent,
+    CrearlibroComponent,
+    DetallelibroComponent,
+    EditarlibroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
